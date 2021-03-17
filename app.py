3564af2f38fe73a73d379f0a106de327a99c581e
panjanageswararao <75623681+panjanageswararao@users.noreply.github.com>
2021-03-17 17:29:52 +0530

    commit

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 import streamlit as st
 from PIL import Image
 import pytesseract as pt
-import pytesseract
-import cv2
-from textblob import TextBlob
 import cv2 as cv
+import cv2
+import pytesseract
+import matplotlib.pyplot as plt 
+from pytesseract import Output
 import numpy as np
-import re
-import matplotlib.pyplot as plt
-pt.pytesseract.tesseract_cmd = '/app/.apt/usr/bin/tesseract'
+import easyocr
+reader = easyocr.Reader(['en'])
+#pt.pytesseract.tesseract_cmd = '/app/.apt/usr/bin/tesseract'
 pytesseract.pytesseract.tesseract_cmd = r'c:\Users\Panja\AppData\Local\Programs\Tesseract-OCR\\tesseract.exe'
-st.set_option('deprecation.showfileUploaderEncoding', False)
-st.sidebar.markdown("""<img style=' align:center;  display: block;margin-left: auto;margin-right: auto;width: 50%;' src="https://media-fastly.hackerearth.com/media/companies/e8e3f0f-he.png">""",unsafe_allow_html=True)
+
 st.sidebar.markdown("""<style>body {background-color: #2C3454;color:white;}</style><body></body>""", unsafe_allow_html=True)
-st.markdown("""<h1 style='text-align: center; color: white;font-size:60px;margin-top:-50px;'>HACKEREARTH</h1><h1 style='text-align: center; color: white;font-size:30px;margin-top:-30px;'>Machine Learning Challenge<br>LOVE is LOVE</h1>""",unsafe_allow_html=True)
+st.markdown("""<h1 style='text-align: center; color: white;font-size:60px;margin-top:-50px;'>CROWDSHAKTI</h1><h1 style='text-align: center; color: white;font-size:30px;margin-top:-30px;'>Machine Learning <br></h1>""",unsafe_allow_html=True)
 
 def intro():
     st.markdown("""<h2 style='text-align: left; color: white;'>Problem Statement</h2><p style='color: white;'>Love knows no gender and the LGBTQ (Lesbian, Gay, Bisexual, Transgender, and Queer) community is the epitome of this thought. <br>In honor of Pride Month, we are here with another Machine Learning challenge, in association with Pride Circle, to celebrate the impact and changes that they made globally.<br>You have been appointed as a social media moderator for your firm. <br>Your key responsibility is to tag and categorize quotes that are uploaded during Pride Month on the basis of its sentiment—positive, negative, and random. <br>Your task is to build a sophisticated Machine Learning model combining Optical Character Recognition (OCR) and Natural Language Processing (NLP) to assess sentiments of these quotes.</p>""",unsafe_allow_html=True)
@@ -29,112 +29,51 @@ st.sidebar.markdown("<h1 style='text-align: center;color: #2C3454;margin-top:30p
 image_file = st.sidebar.file_uploader("", type=["jpg","png","jpeg"])
 
 
-def sentiments(p):
-    if(p>0):
-        return("Positive")
-    elif(p<0):
-        return("Negative")
-    else:
-        return("Random")
 
 def extract(img):
-    slide=st.sidebar.slider("Select Page Segmentation Mode",1,14)
-    conf=f"-l eng --oem 3 --psm {slide}"
-    text=pt.image_to_string(img, config = conf)
-    if(text!=""):
-        st.markdown("<h1 style='color:yellow;'>Extracted Text</h1>", unsafe_allow_html=True)
-        slot1=st.empty()
-        slot2=st.empty()
-        if(st.sidebar.checkbox("Apply Spelling Correction")):
-            corrected=TextBlob(text).correct()
-            slot1.markdown(f"{corrected}", unsafe_allow_html=True)
-            polar=round(corrected.sentiment.polarity,2)
-            slot2.markdown(f"""<h1 style='color:yellow;'>Polarity: <span style='color:white;'>{polar}</span> Sentiment: <span style='color:white;'>{sentiments(polar)}</span></h1>""", unsafe_allow_html=True)
-        if(st.sidebar.checkbox("Remove Numbers & Special Characters")):
-            filtered=re.sub(r'[^A-Za-z ]+', '', text)
-            slot1.markdown(f"{filtered}", unsafe_allow_html=True)
-            polar=round(TextBlob(filtered).sentiment.polarity,2)
-            slot2.markdown(f"""<h1 style='color:yellow;'>Polarity: <span style='color:white;'>{polar}</span> Sentiment: <span style='color:white;'>{sentiments(polar)}</span></h1>""", unsafe_allow_html=True)
-        else:
-            slot1.markdown(f"{text}", unsafe_allow_html=True)
-            polar=round(TextBlob(text).sentiment.polarity,2)
-            slot2.markdown(f"""<h1 style='color:yellow;'>Polarity: <span style='color:white;'>{polar}</span> Sentiment: <span style='color:white;'>{sentiments(polar)}</span></h1>""", unsafe_allow_html=True)
-    else:
-        st.markdown("""<h1 style='text-align: center;'>TEXTLESS IMAGE</h1>""",unsafe_allow_html=True)
-def plot(name,value):
-    st.markdown(f"""<h1 style='text-align: center;'>{name} OUTPUT</h1>""",unsafe_allow_html=True)
-    plt.imshow(value,'gray')
-    thplot="plot.png"
-    plt.savefig(thplot, transparent=True)
-    st.image('plot.png',width=600)
-    extract(value)
-
+    res=reader.readtext(img)
+    for (bbox, text, prob) in res:
+        (tl, tr, br, bl) = bbox
+        tl = (int(tl[0]), int(tl[1]))
+        tr = (int(tr[0]), int(tr[1]))
+        br = (int(br[0]), int(br[1]))
+        bl = (int(bl[0]), int(bl[1]))
+        
+        cv2.rectangle(img, tl, br, (0, 255, 0), 5)
+            
 if image_file is not None:
     st.markdown("<h1 style='color:yellow;'>Uploaded Image</h1>", unsafe_allow_html=True)
     st.image(image_file,width=400)
     file_bytes = np.asarray(bytearray(image_file.read()), dtype=np.uint8)
-    radio=st.sidebar.radio("Select Action",('Text Extraction','Thresholding'))
-    img = cv.imdecode(file_bytes, cv.IMREAD_COLOR)
-    if(radio=="Text Extraction"):
+    radio=st.sidebar.radio("Select Action",('EasyOCR','Pytesseract'))
+    img = cv.imdecode( file_bytes, cv.IMREAD_COLOR)
+    st.markdown("<h1 style='color:yellow;'>Extracted Image</h1>", unsafe_allow_html=True)
+    if (radio =='EasyOCR'):
         extract(img)
+        st.image(img, use_column_width=True,clamp = True)
+       
     else:
-        types=['Binary','Binary Inverse','Truncate','Tozero','Tozero Inverse','Global Thresholding','Adaptive Mean Thresholding', 'Adaptive Gaussian Thresholding']
-        thresh=st.sidebar.selectbox("Select Threshold Type",types)
-        if(thresh==types[0]):
-            if(st.sidebar.checkbox("Grayscale")):
-                img = cv.imdecode(file_bytes, cv.IMREAD_GRAYSCALE)
-            slide=st.sidebar.slider("Select threshold",0,255)
-            ret,th = cv.threshold(img,slide,255,cv.THRESH_BINARY)
-            plot(thresh,th)
-        elif(thresh==types[1]):
-            if(st.sidebar.checkbox("Grayscale")):
-                img = cv.imdecode(file_bytes, cv.IMREAD_GRAYSCALE)
-            slide=st.sidebar.slider("Select threshold",0,255)
-            ret,th = cv.threshold(img,slide,255,cv.THRESH_BINARY_INV)
-            plot(thresh,th)
-        elif(thresh==types[2]):
-            if(st.sidebar.checkbox("Grayscale")):
-                img = cv.imdecode(file_bytes, cv.IMREAD_GRAYSCALE)
-            slide=st.sidebar.slider("Select threshold",0,255)
-            ret,th = cv.threshold(img,slide,255,cv.THRESH_TRUNC)
-            plot(thresh,th)
-        elif(thresh==types[3]):
-            if(st.sidebar.checkbox("Grayscale")):
-                img = cv.imdecode(file_bytes, cv.IMREAD_GRAYSCALE)
-            slide=st.sidebar.slider("Select threshold",0,255)
-            ret,th = cv.threshold(img,slide,255,cv.THRESH_TOZERO)
-            plot(thresh,th)
-        elif(thresh==types[4]):
-            if(st.sidebar.checkbox("Grayscale")):
-                img = cv.imdecode(file_bytes, cv.IMREAD_GRAYSCALE)
-            slide=st.sidebar.slider("Select threshold",0,255)
-            ret,th = cv.threshold(img,slide,255,cv.THRESH_TOZERO_INV)
-            plot(thresh,th)
-        elif(thresh==types[5]):
-            if(st.sidebar.checkbox("Grayscale")):
-                img = cv.imdecode(file_bytes, cv.IMREAD_GRAYSCALE)
-            img = cv.medianBlur(img,5)
-            slide=st.sidebar.slider("Select threshold",0,255)
-            ret,th = cv.threshold(img,slide,255,cv.THRESH_BINARY)
-            plot(thresh,th)
-        elif(thresh==types[6]):
-            img = cv.medianBlur(img,5)
-            img = cv.imdecode(file_bytes, cv.IMREAD_GRAYSCALE)
-            th = cv.adaptiveThreshold(img,255,cv.ADAPTIVE_THRESH_MEAN_C,\
-            cv.THRESH_BINARY,11,2)
-            plot(thresh,th)
-        elif(thresh==types[-1]):
-            img = cv.medianBlur(img,5)
-            img = cv.imdecode(file_bytes, cv.IMREAD_GRAYSCALE)
-            th = cv.adaptiveThreshold(img,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C,\
-            cv.THRESH_BINARY,11,2)
-            plot(thresh,th)
-else:
-    intro()
-hide_streamlit_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+        
+        d = pytesseract.image_to_data(img,output_type=Output.DICT)
+        n_boxes = len(d['level'])
+        for i in range(n_boxes):
+            if(d['text'][i] != ""):
+                (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
+                cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        plt.imshow(img)
+        st.image(img, use_column_width=True,clamp = True)
+        
+
+   
+  
+
+
+  
+            
+    
+
+   
+
+
+
